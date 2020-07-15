b0046fa31038549490578c4b8f53ff92fac9a410
liubianshi_laptop <liu.bian.shi@gmail.com>
2020-07-15 22:47:03 +0800

    上传github之前

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;    /* Switch view on tag switch */
 static const char *fonts[]          = { 
     "Fira Code:pixelsize=24:antialias=true:autohint:false:hint:ture:hintstyle:hintfull",
@@ -58,10 +58,8 @@ static const Rule rules[] = {
 	/* class          instance    title       tags mask     isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",         NULL,       NULL,       0,            1,          0,           0,        -1 },
 	{ "Firefox",      NULL,       NULL,       1 << 8,       0,          0,          -1,        -1 },
-    { "qutebrowser",  NULL,       NULL,       1 << 1,       0,          0,          -1,        -1 },
-	{ "st",           NULL,       NULL,       0,            0,          1,          -1,        -1 },
+	{ "St",           NULL,       NULL,       0,            0,          1,          -1,        -1 },
 	{ NULL,           NULL,  "Event Tester",  0,            1,          0,           1,        -1 }, /* xev */
-    { "Gimp",         NULL,       NULL,       0,            0,          0,           0,        -1 },
     { "fzfmenu",      NULL,       NULL,       0,            1,          0,           1,        -1 },
     { "ncmpcpp",      NULL,       NULL,       0,            1,          0,           1,        -1 },
     { "blueman-manager", NULL,    NULL,       0,            1,          0,           1,        -1 },
@@ -71,7 +69,7 @@ static const Rule rules[] = {
     { "Wps",          NULL,       NULL,       1 << 2,       0,          0,           0,        -1 },
     { "Zenity",       NULL,       NULL,       0,            1,          0,           1,        -1 },
     { "R_x11",        NULL,       NULL,       0,            1,          0,           1,        -1 },
-
+    { "qutebrowser",  NULL,       NULL,       1 << 1,       0,          0,          -1,        -1 },
 };
 
 /* layout(s) */
@@ -115,7 +113,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x24", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "127x20+300+0", NULL };
 static Key keys[] = {
     /* modifier                      key        function        argument */
     STACKKEYS(MODKEY,           focus)
